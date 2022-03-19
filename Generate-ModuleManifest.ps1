a203f1a63e05466d4721dfa8d7871b7008d44ea5
Darren Hollinrake <hollinrakedp@gmail.com>
2022-03-19 06:39:44 -0500

    Rename and Update Generate-ModuleManifest.ps1

---
+++ Generate-ModuleManifest.ps1
@@ -0,0 +1,19 @@
+$ModuleName = $PSScriptRoot | Split-Path -Leaf
+$FunctionsToExport = @( (Get-ChildItem -Path $PSScriptRoot\$ModuleName\Public\*.ps1).BaseName )
+
+$manifest = @{
+    Path              = ".\$ModuleName\$ModuleName.psd1"
+    RootModule        = "$ModuleName.psm1"
+    Author            = 'Darren Hollinrake'
+    Company           = 'Darren Hollinrake'
+    Description       = 'Collection of functions for configuration and management of auditing a Windows system.'
+    ModuleVersion     = '0.2'
+    FunctionsToExport = $FunctionsToExport
+}
+
+if (Test-Path $manifest.Path){
+    Update-ModuleManifest @manifest
+}
+else {
+    New-ModuleManifest @manifest
+}
